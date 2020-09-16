f4d857f9af9c26ac625118991049b1fda8fbb2d9
joaoptgaino <joaoptgaino@hotmail.com>
2020-09-15 21:20:48 -0300

    added the category handler at the routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,12 @@
 
 import express from 'express';
+import CategoryController from './controllers/CategoryController';
 import IncomeController from './controllers/IncomeController';
 
 const routes = express.Router();
 
 const incomeController = new IncomeController();
-
-routes.get('/', (req, res) => {
-    res.json({
-        message: "Hello from routes"
-    });
-});
-
+const categoryController = new CategoryController();
 //INCOME
 routes.get('/incomes',incomeController.index);
 routes.post('/incomes',incomeController.create);
@@ -19,6 +14,10 @@ routes.put('/incomes/:id',incomeController.update);
 routes.delete('/incomes/:id',incomeController.delete);
 
 
-//EXPENSES
+//CATEGORY
+routes.get('/category',categoryController.index);
+routes.post('/category',categoryController.create);
+routes.put('/category/:id',categoryController.update);
+routes.delete('/category/:id',categoryController.delete);
 
 export default routes;
